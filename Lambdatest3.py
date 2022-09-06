4e7505e25eb48677e0617c385d4a6f299f0c0853
sandeepmaddalaa <85775052+sandeepmaddalaa@users.noreply.github.com>
2022-09-06 11:44:28 +0530

    Create Lambdatest3.py

---
+++ Lambdatest3.py
@@ -0,0 +1,38 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome(executable_path='C:\\Driver\\chromedriver.exe')
+
+driver.get('https://www.lambdatest.com/selenium-playground')
+
+
+driver.find_element(By.PARTIAL_LINK_TEXT, "Input Form Submit").click()
+driver.find_element(By.CLASS_NAME, "btn").click()
+assert "Please fill in the fields"
+
+driver.find_element(By.ID, "name").send_keys("Sandeep")
+driver.find_element(By.ID, "inputEmail4").send_keys("sandeepmaddalaa@gmail.com")
+driver.find_element(By.ID, "inputPassword4").send_keys("Sandeep@143")
+driver.find_element(By.ID, "company").send_keys("Mindtree")
+driver.find_element(By.ID, "websitename").send_keys("Lambdatest.com")
+city = driver.find_element(By.XPATH, '//*[@id="seleniumform"]/div[3]/div[1]/select')
+dropdown = Select(city)
+dropdown.select_by_visible_text("India")
+driver.find_element(By.ID, "inputCity").send_keys("sr nagar")
+driver.find_element(By.ID, "inputAddress1").send_keys("near mindtree")
+driver.find_element(By.ID, "inputAddress2").send_keys("HYd")
+driver.find_element(By.XPATH, '//*[@id="inputState"]').send_keys("telengana")
+driver.find_element(By.ID, "inputZip").send_keys("500038")
+time.sleep(2)
+
+
+driver.find_element(By.CLASS_NAME, "btn").click()
+
+Thanks = driver.find_element(By.XPATH, '//*[@id="__next"]/div/section[3]/div/div/div[2]/div/p').text
+print(Thanks)
+
+driver.close()
